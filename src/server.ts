6cfd69c231ae4e8806a2c445ff7c84a222bcf06b
ashimroyrupok <ashimroyrupok@gmail.com>
2024-05-22 03:39:18 +0600

    succesfully conneted with mongodb atlas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 
-app.listen(5000, () => {
-  console.log(`Example app listening on port ${5000}`);
-});
+
+async function main() {
+
+    try {
+         await mongoose.connect(config.db_uri as string);
+         app.listen(config.port, () => {
+           console.log(`Example app listening on port ${config.port}`);
+         });
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+main();
+
